19d39281a7124ea61a81d7ed0d54d98426e9b355
leandl <leandrocrispim891@gmail.com>
2026-09-21 12:51:01 -0300

    refactor: add BankRepositoryDatabase

---
+++ src/bank-repository-database.ts
@@ -0,0 +1,93 @@
+import { Bank } from '@bank.ts'
+import { BankDAO } from '@bank-dao.ts'
+import { BankRepository } from '@bank-repository.ts'
+
+export class BankRepositoryDatabase implements BankRepository {
+  constructor(private bankDAO: BankDAO) {}
+
+  async save(bank: Bank): Promise<Bank> {
+    const bankId = await this.bankDAO.save({
+      code: bank.getCode(),
+      name: bank.getName(),
+      url: bank.getUrl(),
+    })
+
+    return Bank.restore({
+      id: bankId,
+      code: bank.getCode(),
+      name: bank.getName(),
+      url: bank.getUrl(),
+    })
+  }
+
+  async findById(bankId: number): Promise<Bank | undefined> {
+    const bankDTO = await this.bankDAO.getById(bankId)
+
+    if (!bankDTO) {
+      return undefined
+    }
+
+    return Bank.restore({
+      id: bankDTO.bank_id,
+      code: bankDTO.code,
+      name: bankDTO.name,
+      url: bankDTO.url,
+    })
+  }
+
+  async findByCode(code: string): Promise<Bank | undefined> {
+    const bankDTO = await this.bankDAO.getByCode(code)
+
+    if (!bankDTO) {
+      return undefined
+    }
+
+    return Bank.restore({
+      id: bankDTO.bank_id,
+      code: bankDTO.code,
+      name: bankDTO.name,
+      url: bankDTO.url,
+    })
+  }
+
+  async findByName(name: string): Promise<Bank | undefined> {
+    const bankDTO = await this.bankDAO.getByName(name)
+
+    if (!bankDTO) {
+      return undefined
+    }
+
+    return Bank.restore({
+      id: bankDTO.bank_id,
+      code: bankDTO.code,
+      name: bankDTO.name,
+      url: bankDTO.url,
+    })
+  }
+
+  async update(bank: Bank): Promise<void> {
+    await this.bankDAO.update({
+      id: bank.getBankId(),
+      code: bank.getCode(),
+      name: bank.getName(),
+      url: bank.getUrl(),
+    })
+  }
+
+  async list(): Promise<Bank[]> {
+    const bankDTOs = await this.bankDAO.list()
+
+    return bankDTOs.map((bankDTO) =>
+      Bank.restore({
+        id: bankDTO.bank_id,
+        code: bankDTO.code,
+        name: bankDTO.name,
+        url: bankDTO.url,
+      }),
+    )
+  }
+
+  async remove(bankId: number): Promise<void> {
+    await this.bankDAO.remove(bankId)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { ApplicationStatusRestController } from '@application-status-rest-controller.ts'
-import { BankRepositorySQL } from '@bank-repository.ts'
+import { BankDAODatabase } from '@bank-dao.ts'
+import { BankRepositoryDatabase } from '@bank-repository-database.ts'
 import { BankRestController } from '@bank-rest-controller.ts'
 import { CreateBank } from '@create-bank.ts'
 import { ExpressAdapter } from '@express-adapter.ts'
-import { FastifyAdapter } from '@fastify-adapter.ts'
 import { GetApplicationStatus } from '@get-application-status.ts'
 import { GetBankById } from '@get-bank-by-id.ts'
 import { GetBankList } from '@get-bank-list.ts'
@@ -14,18 +14,19 @@ import { UpdateBank } from '@update-bank.ts'
 const databaseConnection = new MysqlAdapter(
   String(process.env.DATABASE_MYSQL_URL),
 )
-
 // const databaseConnection = new PostgresAdapter(
 //   String(process.env.DATABASE_POSTGRES_URL),
 // )
-
 // const databaseConnection = new SqliteAdapter(
 //   String(process.env.DATABASE_SQLITE_FILENAME),
 // )
 
-const bankRepository = new BankRepositorySQL(databaseConnection)
+const bankDAO = new BankDAODatabase(databaseConnection)
+const bankRepository = new BankRepositoryDatabase(bankDAO)
+// const bankRepository = new BankRepositorySQL(databaseConnection)
+
 const httpRestServer = new ExpressAdapter()
-const _httpRestServer = new FastifyAdapter()
+// const httpRestServer = new FastifyAdapter()
 
 const getBankList = new GetBankList(bankRepository)
 const getBankById = new GetBankById(bankRepository)
